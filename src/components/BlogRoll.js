22003861fe95590a6c6062af6ef031642859bf26
gleyconxavier <gleyconxcarlos@gmail.com>
2020-01-12 23:06:51 -0400

    Updates

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -24,7 +24,7 @@ class BlogRoll extends React.Component {
                       <PreviewCompatibleImage
                         imageInfo={{
                           image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                          alt: `thumbnail relativa ao post ${post.frontmatter.title}`,
                         }}
                       />
                     </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="https://gleyconxavier.netlify.com/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content="https://gleyconxavier.netlify.com/img/og-image.jpg"
         />
       </Helmet>
       <Navbar />
